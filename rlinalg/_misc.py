9a0a9ad1a2c88ae4f833e3b52a2b5fdff22d5b5a
Martin Larralde <martin.larralde@embl.de>
2024-07-03 16:43:24 +0200

    Add interfaces to `dqrdc2` and `dqrqy` in `rlinalg.linpack`

---
+++ rlinalg/_misc.py
@@ -0,0 +1,13 @@
+import numpy 
+
+def _datacopied(arr, original):
+    """
+    Strict check for `arr` not sharing any data with `original`,
+    under the assumption that arr = asarray(original)
+
+    """
+    if arr is original:
+        return False
+    if not isinstance(original, numpy.ndarray) and hasattr(original, "__array__"):
+        return False
+    return arr.base is None
--- rlinalg/linpack/__init__.py
+++ rlinalg/linpack/__init__.py
@@ -1,20 +1,9 @@
 import numpy
 from numpy import atleast_2d
 
+from .._misc import _datacopied
 from ._dqrdc2 import dqrdc2 as _dqrdc2
-
-
-def _datacopied(arr, original):
-    """
-    Strict check for `arr` not sharing any data with `original`,
-    under the assumption that arr = asarray(original)
-
-    """
-    if arr is original:
-        return False
-    if not isinstance(original, numpy.ndarray) and hasattr(original, "__array__"):
-        return False
-    return arr.base is None
+from ._dqrutl import dqrqy as _dqrqy
 
 
 def _asarray_validated(
@@ -73,7 +62,12 @@ def _asarray_validated(
     return a
 
 
-def dqrdc2(a, tol=1e-12, overwrite_a=False):
+def dqrdc2(
+    a, 
+    tol=1e-12,
+    overwrite_a=False,
+    check_finite=True,
+):
     """
     Wrapper for ``dqrdc2``.
 
@@ -93,7 +87,7 @@ def dqrdc2(a, tol=1e-12, overwrite_a=False):
 
     """
 
-    a1 = atleast_2d(_asarray_validated(a, check_finite=False, order='F', dtype=numpy.double))
+    a1 = atleast_2d(_asarray_validated(a, check_finite=check_finite, order='F', dtype=numpy.double))
     overwrite_a = overwrite_a or _datacopied(a1, a)
 
     n, p = a1.shape
@@ -113,3 +107,21 @@ def dqrdc2(a, tol=1e-12, overwrite_a=False):
         info = 0
 
     return (a1, k, jpvt, tau, work, info)
+
+
+def dqrqy(
+    qr,
+    tau,
+    a,
+    overwrite_a=False,
+    check_finite=True,
+):
+    qr1 = atleast_2d(_asarray_validated(qr, check_finite=check_finite, order='F', dtype=numpy.double))
+    a1 = atleast_2d(_asarray_validated(a, check_finite=check_finite, order='F', dtype=numpy.double))
+    
+    overwrite_a = overwrite_a or _datacopied(a1, a)
+    if not overwrite_a:
+        a1 = numpy.array(a1, order='F', dtype=numpy.double)
+
+    _dqrqy(qr1, qr1.shape[0], qr1.shape[1], tau, a1, a1.shape[0], a1)
+    return a1
